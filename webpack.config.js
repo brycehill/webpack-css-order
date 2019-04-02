481a169f7e44b8ec1adab819574cd88299edac8c
Bryce Hill <bryce@clearvoice.com>
2019-04-02 11:16:14 -0700

    Upgrade to webpack 4.12.2 - GOOD

--- webpack.config.js
+++ webpack.config.js
@@ -12,11 +12,12 @@ module.exports = () => {
           {
             test: /\.js$/,
             exclude: /node_modules/,
-            use: [
-              {
-                loader: "babel-loader"
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/react']
               }
-            ]
+            }
           },
           {
             test: /\.css$/,
